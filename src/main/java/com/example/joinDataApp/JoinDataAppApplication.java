593b67bc4416a48dbd96442865956ffca2711778
MILOSZ <milosz.siegmund@gmail.com>
2020-10-15 16:10:36 +0200

    initial commit

---
+++ src/main/java/com/example/joinDataApp/JoinDataAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.joinDataApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoinDataAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoinDataAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/joinDataApp/JoinDataAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.joinDataApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoinDataAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
